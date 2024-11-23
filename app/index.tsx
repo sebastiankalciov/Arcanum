551cb3dba5f88e95566f8da2d559310105df1d51
Sebastian Kalciov <sebastian.kalciov@gmail.com>
2024-11-23 18:41:28 +0200

    Fix development build issue, add basic AR scene

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,58 @@
-import { Text, View } from "react-native";
+import {StyleSheet, Text, View} from "react-native";
+import {useState} from "react";
+import {
+    ViroARScene,
+    ViroARSceneNavigator,
+    ViroText,
+    ViroTrackingReason,
+    ViroTrackingStateConstants,
+    ViroFlexView,
+    ViroImage
+} from "@reactvision/react-viro";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+
+    const HelloWorldSceneAR = () => {
+        const [text, setText] = useState("Initializing AR...");
+
+        function onInitialized(state: any, reason: ViroTrackingReason) {
+            console.log("guncelleme", state, reason);
+            if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+                setText("hai noroci");
+            } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
+                // Handle loss of tracking
+            }
+        }
+
+        return (
+            <ViroARScene onTrackingUpdated={onInitialized}>
+                <ViroText
+                    text={text}
+                    scale={[0.5, 0.5, 0.5]}
+                    position={[0, 0, -1]}
+                    style={styles.helloWorldTextStyle}
+                />
+
+            </ViroARScene>
+        );
+    };
+    return (
+        <ViroARSceneNavigator
+            autofocus={true}
+            initialScene={{
+                scene: HelloWorldSceneAR,
+            }}
+            style={styles.f1}
+        />
+    );
 }
+var styles = StyleSheet.create({
+    f1: { flex: 1 },
+    helloWorldTextStyle: {
+        fontFamily: "Arial",
+        fontSize: 70,
+        color: "#f92929",
+        textAlignVertical: "center",
+        textAlign: "center",
+    },
+});
